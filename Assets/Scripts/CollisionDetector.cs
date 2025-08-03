2ef9c1a012b95a7aaa7b59be6388d0f67ffdf712
Nartiohk <khoitran1234z@gmail.com>
2025-08-03 12:38:24 +0700

    Need Background Music

--- Assets/Scripts/CollisionDetector.cs
+++ Assets/Scripts/CollisionDetector.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class CollisionDetector : MonoBehaviour
 {
     [SerializeField] private AudioClip[] diedClips;
+    [SerializeField] private GameOverMenu gameOverMenu; // Assign in Inspector
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
@@ -10,7 +12,12 @@ public class CollisionDetector : MonoBehaviour
             Debug.Log("Trigger entered with: " + other.name);
             // Play a random sound from the diedClips array
             SoundFXManager.instance.PlayRandomSound(diedClips, transform, 1f);
-        }
+            
+            if (gameOverMenu != null)
+            {
+                gameOverMenu.Show();
+            }
+        }   
 
     }
 }
---
+++ Assets/Scripts/GameOverMenu.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.SceneManagement;
+
+public class GameOverMenu : MonoBehaviour
+{
+    void Start()
+    {
+        Hide();
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public void Show()
+    {
+        gameObject.SetActive(true);
+        Time.timeScale = 0f; // Pause the game
+    }
+    public void Hide()
+    {
+        gameObject.SetActive(false);
+        Time.timeScale = 1f; // Resume the game
+    }
+
+    public void Again()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
